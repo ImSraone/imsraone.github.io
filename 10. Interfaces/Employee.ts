784bb5726435a3d4d48a7b6ee086cd240aa2128f
Karunakar Bhogyari <Karunakar.Bhogyari@KPMG.co.uk>
2020-07-09 03:12:15 +0100

    Added more examples of interfaces

--- 10. Interfaces/Employee.ts
+++ 10. Interfaces/Employee.ts
@@ -6,6 +6,12 @@ interface IEmployee {
 }
 
 class Employee implements IEmployee {
+  constructor(empId: number, Name: string, Location: string, Salary?: number) {
+    this.EmployeeId = empId;
+    this.EmployeeName = Name;
+    this.Location = Location;
+    this.Salary = Salary;
+  }
   EmployeeId: number;
   EmployeeName: string;
   Location: string;
@@ -21,3 +27,6 @@ class Employee implements IEmployee {
 }
 
 export default Employee;
+
+const emp = new Employee(10, "Karuanakr", "Bangalore");
+console.log(emp.EmployeeName);
